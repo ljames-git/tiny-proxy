9d209215e30631dff445c0f439ed9902a82588de
ljames-git <lsadw@163.com>
2017-11-29 18:27:33 +0800

    add code

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char ** argv)
 {
     //CTcpServer *s = new CTcpServer(9898);
     CTcpServer *s = new CHttpServer;
-    s->get_model()->set_timeout(1000);
+    //s->get_model()->set_timeout(1000);
 
     if (s->start() == 0)
         s->get_model()->start();
