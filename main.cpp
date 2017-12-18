b362371861418ea3c42091965b5bfc675f9b664b
ljames-git <lsadw@163.com>
2017-12-18 17:20:17 +0800

    add code

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
-#include "curl/curl.h"
+#include "common.h"
 #include "TcpServer.h"
 #include "HttpServer.h"
 
+#ifndef MULTI_THREAD_VERSION
 int main(int argc, char ** argv)
 {
-    //CTcpServer *s = new CTcpServer(9898);
-    CTcpServer *s = new CHttpServer(8888);
-    //s->get_model()->set_timeout(1000);
+    int port = 8888;
+    CTcpServer *s = new CHttpServer(port);
+    if (s == NULL)
+    {
+        LOG_ERROR("failed to create httpserver, bind port: %d", port);
+        return -1;
+    }
 
-    if (s->start() == 0)
-        s->get_model()->start();
+    if (s->start() == 0 && s->get_model()->start() == 0)
+    {
+        LOG_INFO("HTTP SERVER START SUCCESSFULLY");
+    }
+    else
+    {
+        LOG_ERROR("HTTP SERVER START ERROR");
+        return -2;
+    }
+
     return 0;
 }
+#else
+#endif //MULTI_THREAD_VERSION
